49b230e5db7f5f916f09854a982b6188c2f14a8b
superflytnt <seth@seth.org>
2023-04-27 22:00:43 -0400

    Update wled_poly.py

--- wled_poly.py
+++ wled_poly.py
@@ -6,7 +6,7 @@ based on the NodeServer template for Polyglot v2 written in Python2/3 by Einstei
 Using this Python Library to control WLED by https://github.com/pctechjon/wledpy
 """
 
-import polyinterface
+import udi_interface
 import time
 import json
 import sys
@@ -15,7 +15,7 @@ import zipfile
 from wled import Wled
 from threading import Thread
 
-LOGGER = polyinterface.LOGGER
+LOGGER = udi_interface.LOGGER
 
 with open('server.json') as data:
     SERVERDATA = json.load(data)
@@ -25,56 +25,68 @@ except (KeyError, ValueError):
     LOGGER.info('Version not found in server.json.')
     VERSION = '0.0.0'
 
-class Controller(polyinterface.Controller):
+class Controller(udi_interface.Node):
 
-    def __init__(self, polyglot):
-        super(Controller, self).__init__(polyglot)
+    def __init__(self, polyglot, primary, address, name):
+        super(Controller, self).__init__(polyglot, primary, address, name)
+        self.poly = polyglot
         self.discovery_thread = None
         self.do_poll = False
         self.name = 'WLED'
         self.myHost = None
         self.hb = 0
+
+        polyglot.subscribe(polyglot.START, self.start, address)
+        polyglot.subscribe(polyglot.CUSTOMPARAMS, self.parameterHandler)
+        polyglot.subscribe(polyglot.POLL, self.poll)
+
+        polyglot.ready()
+        polyglot.addNode(self)
         
-    def start(self):
-        LOGGER.info('Started WLED for v2 NodeServer version %s', str(VERSION))
+    def parameterHandler(self, params):
+        self.poly.Notices.clear()
         try:
             
             # Get and set IP
-            if 'host' in self.polyConfig['customParams'] :
-                self.myHost = self.polyConfig['customParams']['host']
+            if 'host' in params :
+                self.myHost = params['host']
                 LOGGER.info('Custom IP address specified: {}'.format(self.myHost))
             else:
                 LOGGER.error('Need to have ip address in custom param host')
                 self.setDriver('ST', 0)
                 return False                
                         
-            self.setDriver('ST', 1)
-            self.discover()
+            if self.myHost != "":
+                self.setDriver('ST', 1)
+                self.discover()
                                                             
         except Exception as ex:
             LOGGER.error('Error starting WLED NodeServer: %s', str(ex))
             self.setDriver('ST', 0)
             return False
 
-    def shortPoll(self):
-        if self.discovery_thread is not None:
-            if self.discovery_thread.is_alive():
-                LOGGER.debug('Skipping shortPoll() while discovery in progress...')
-                return
-            else:
-                self.discovery_thread = None
-        
-        self.setDriver('ST', 1)
-        for node in self.nodes:
-            if  self.nodes[node].do_poll:
-                self.nodes[node].update()
+    def start(self):
+        LOGGER.info('Started WLED for v2 NodeServer version %s', str(VERSION))
 
-    def longPoll(self):
-        self.heartbeat()
+    def poll(self, polltype):
+        if 'shortPoll' in polltype:
+            if self.discovery_thread is not None:
+                if self.discovery_thread.is_alive():
+                    LOGGER.debug('Skipping shortPoll() while discovery in progress...')
+                    return
+                else:
+                    self.discovery_thread = None
+        
+            self.setDriver('ST', 1)
+            for node in self.poly.nodes():
+                if  node.do_poll:
+                    node.update()
+        else:
+            self.heartbeat()
 
     def query(self):
-        for node in self.nodes:
-            self.nodes[node].reportDrivers()
+        for node in self.poly.nodes():
+            node.reportDrivers()
         
     def heartbeat(self):
         LOGGER.debug('heartbeat: hb={}'.format(self.hb))
@@ -108,7 +120,8 @@ class Controller(polyinterface.Controller):
         lstIp = self.myHost.split(',')
         id = 1
         for ip in lstIp:
-            self.addNode(WledNode(self, self.address, 'wled' + str(id) , 'wled' + str(id), ip))
+            if not self.poly.getNode('wled' + str(id)):
+                self.poly.addNode(WledNode(self.poly, self.address, 'wled' + str(id) , 'wled' + str(id), ip))
             id = id + 1 
 
     def delete(self):
@@ -118,10 +131,11 @@ class Controller(polyinterface.Controller):
     commands = {'DISCOVERY' : runDiscover}
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 2}]
     
-class WledNode(polyinterface.Node):
+class WledNode(udi_interface.Node):
 
     def __init__(self, controller, primary, address, name, ip):
         super(WledNode, self).__init__(controller, primary, address, name)
+        self.poly = controller
         self.do_poll = True
         self.wled_ip = ip
         self.arrEffects = None
@@ -137,8 +151,6 @@ class WledNode(polyinterface.Node):
         self.__saveEffetsList()
         self.__BuildProfile()
 
-    def start(self):
-        pass
         
     def setOn(self, command):
         self.my_wled.turn_on()
@@ -147,7 +159,7 @@ class WledNode(polyinterface.Node):
     def setOff(self, command):
         self.my_wled.turn_off()
         self.setDriver('ST', 0)
-             
+        
     def setBrightness(self, command):
         intBri = int(command.get('value'))
         self.my_wled.set_brightness(intBri)                                            
@@ -175,7 +187,7 @@ class WledNode(polyinterface.Node):
         self.setDriver('GV6', color_r)
         self.setDriver('GV7', color_g)
         self.setDriver('GV8', color_b)
-    
+
     def setCustomAPI(self, command):
 	    api_command = command.get('value')
 	    # Make a request to the WLED device using the custom API command
@@ -185,7 +197,7 @@ class WledNode(polyinterface.Node):
 	        LOGGER.info(f'Successfully sent custom API command: {api_command}')
 	    else:
 	        LOGGER.error(f'Failed to send custom API command: {api_command}')
-    
+        
     def setProfile(self, command):
         self.__saveEffetsList()
         self.__BuildProfile()
@@ -273,7 +285,7 @@ class WledNode(polyinterface.Node):
         except Exception as ex:
             LOGGER.error('Error generating profile: %s', str(ex))
         
-        self.parent.install_profile()
+        self.poly.updateProfile()
 
         
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 78},
@@ -284,24 +296,23 @@ class WledNode(polyinterface.Node):
                {'driver': 'GV8', 'value': 0, 'uom': 100}]
     
     id = 'WLED'
-                
     commands = {
-				    'QUERY': query,            
-				    'DON': setOn,
-				    'DOF': setOff,
-				    'SET_PROFILE': setProfile,
-				    'SET_BRI': setBrightness,
-				    'SET_EFFECT': setEffect,
-				    'SET_COLORID': setColor,
-				    'SET_CUSTOM_API': setCustomAPI 
-				}
-                
+                    'QUERY': query,            
+                    'DON': setOn,
+                    'DOF': setOff,
+                    'SET_PROFILE' : setProfile,
+                    'SET_BRI': setBrightness,
+                    'SET_EFFECT': setEffect,
+                    'SET_COLORID':setColor
+                }
     
 if __name__ == "__main__":
     try:
-        polyglot = polyinterface.Interface('WledNodeServer')
+        polyglot = udi_interface.Interface([])
         polyglot.start()
-        control = Controller(polyglot)
-        control.runForever()
+        polyglot.updateProfile()
+        polyglot.setCustomParamsDoc()
+        Controller(polyglot, 'controller', 'controller', 'WledNodeServer')
+        polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
